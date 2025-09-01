4033152a5b452aa940e221d53aedc4896039d0ab
touraye <touraye07@gmail.com>
2025-09-01 18:00:57 +0000

    add tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,14 @@ const config: Config = {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+                'dotted-line-y': 'radial-gradient(circle at center, #e5e7eb 1px, transparent 1.5px)',
             },
+            colors: {
+                'brand-teal': {
+                    DEFAULT: '#0f4c5c',
+                    dark: '#0c3b47',
+                },
+            }
         },
     },
     plugins: [],
