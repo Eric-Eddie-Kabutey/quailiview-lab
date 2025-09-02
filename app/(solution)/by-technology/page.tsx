f7558de53ee064268aa6778252b502f76c2a83a6
touraye <touraye07@gmail.com>
2025-09-02 18:44:06 +0000

    add by industry page

---
+++ app/(solution)/by-technology/page.tsx
@@ -0,0 +1,12 @@
+import CTA from "@/components/home/cta";
+
+export default function ByTechnologyPage() {
+    return (<>
+        {/* hero */}
+
+        {/* services */}
+
+        {/* cta */}
+        <CTA />
+    </>)
+}
