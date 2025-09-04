405ac7ec10d18f6450cd3b7670775b9354d51eb8
touraye <touraye07@gmail.com>
2025-09-04 12:14:07 +0000

    add event page

---
+++ app/event/page.tsx
@@ -0,0 +1,11 @@
+import CTA from "@/components/home/cta";
+
+export default function EventPage() {
+    return (<>
+        {/* hero showing featured event */}
+        <h1>Our Events</h1>
+
+        {/* Cta - reusable */}
+        <CTA />
+    </>)
+}
