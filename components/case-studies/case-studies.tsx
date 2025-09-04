0ce9e7292ddbad99a170c173748d4d620e4b6b6e
touraye <touraye07@gmail.com>
2025-09-04 12:58:04 +0000

    fix spacing

--- components/case-studies/case-studies.tsx
+++ components/case-studies/case-studies.tsx
@@ -14,7 +14,7 @@ const containerVariants = {
 export default function CaseStudiesSection() {
   return (
     <section className="bg-slate-50/70 py-24 sm:py-32">
-      <div className="mx-auto lg:max-w-5xl xl:max-w-7xl px-4 sm:px-6">        
+      <div className="mx-auto lg:max-w-5xl xl:max-w-6xl px-4 sm:px-0">        
         <motion.div
           variants={containerVariants}
           initial="hidden"
