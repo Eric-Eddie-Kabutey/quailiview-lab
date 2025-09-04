95967a641b3315ae34699f71d2c539978ddcb2fa
touraye <touraye07@gmail.com>
2025-09-04 19:09:13 +0000

    add case studies

--- app/(insight)/case-studies/page.tsx
+++ app/(insight)/case-studies/page.tsx
@@ -1,14 +1,19 @@
-import CaseStudiesSection from "@/components/case-studies/case-studies";
 import CaseStudiesHero from "@/components/case-studies/hero";
 import CTA from "@/components/home/cta";
+import PaginatedPostGrid from "@/components/PaginatedPostGrid";
+import { mockCaseStudies } from "@/data/mock-case-studies";
 
 export default function BlogPage() {
     return (<>
         {/* hero showing featured event */}
         <CaseStudiesHero />
 
-        {/* blog listing */}        
-        <CaseStudiesSection />
+        {/* blog listing */}                
+        <PaginatedPostGrid 
+          title="Case Studies" 
+          initialPosts={mockCaseStudies} 
+          postType="case-study"
+        />
 
         {/* Cta - reusable */}
         <CTA />
