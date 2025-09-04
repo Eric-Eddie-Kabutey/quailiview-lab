fcf09591b5b097b2fb975c345e04a6e3352c7721
touraye <touraye07@gmail.com>
2025-09-04 19:05:05 +0000

    add types for post and event

---
+++ types/index.ts
@@ -0,0 +1,25 @@
+import { StaticImageData } from 'next/image';
+
+export interface Post {
+    slug: string;
+    title: string;
+    category: string;
+    author: string;
+    publishedDate: string; // CRITICAL: This must be a string for serialization
+    readingTime: number;
+    summary: string;
+    coverImage: string | StaticImageData;
+    content?: React.ReactNode;
+}
+
+// The shape for an Event
+export interface Event {
+    slug: string;
+    title: string;
+    date: string; // CRITICAL: This must be a string for serialization
+    formatAndLocation: string; // A combined string for display
+    summary: string;
+    coverImage: string | StaticImageData;
+    isPast?: boolean; // This is calculated on the server, not passed to client
+    content?: React.ReactNode;
+}
