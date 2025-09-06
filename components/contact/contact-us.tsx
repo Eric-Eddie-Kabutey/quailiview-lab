2becbe3203a660b3be2660bb596585161508f955
touraye <touraye07@gmail.com>
2025-09-06 12:36:49 +0000

    update international phone input

--- components/contact/contact-us.tsx
+++ components/contact/contact-us.tsx
@@ -6,6 +6,8 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import Link from 'next/link'
+import PhoneInput, {isValidPhoneNumber } from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
 
 import { Button } from '@/components/ui/button'
 import {
@@ -25,8 +27,9 @@ import {
 	SelectValue,
 } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
-import { CheckCircle2, Loader2 } from 'lucide-react'
+import { Loader2 } from 'lucide-react'
 import Image from 'next/image'
+import BackgroundEffect from '../shared/background-effect'
 
 // Zod Schema for validation
 const formSchema = z.object({
@@ -34,8 +37,11 @@ const formSchema = z.object({
 		.string()
 		.min(2, { message: 'Full name must be at least 2 characters.' }),
 	email: z.string().email({ message: 'Please enter a valid email address.' }),
-	company: z.string().optional(),
-	phone: z.string().optional(),
+	company: z.string().min(5, {message: "Please enter your company/org name"}),
+	phone: z.string().refine(
+		(val) => isValidPhoneNumber(val), 
+		{ message: "Invalid phone number" }
+	),
 	inquiryType: z.enum(['Project Inquiry', 'Careers', 'Support', 'Other']),
 	message: z
 		.string()
@@ -67,7 +73,7 @@ function ContactForm({ onSuccess }: { onSuccess: () => void }) {
 	}
 
 	return (
-		<div className='bg-[#E4E5E9] p-8 rounded-2xl shadow-lg border border-gray-400'>
+		<div className='bg-[#FFFFFF] p-8 rounded-2xl shadow-lg border border-gray-400'>
 			<h3 className='text-xl text-black font-semibold text-center mb-6'>
 				Talk to an expert
 			</h3>
@@ -119,8 +125,17 @@ function ContactForm({ onSuccess }: { onSuccess: () => void }) {
 							<FormItem>
 								<FormLabel className='text-black font-semibold'>Phone Number</FormLabel>
 								<FormControl>
-									<Input placeholder='+1' {...field} className='border border-gray-400' />
+									
+								 <PhoneInput
+									international
+									defaultCountry="US"
+									placeholder="Enter phone number"
+									value={field.value}
+									onChange={field.onChange}
+									className="w-full text-black font-medium border border-gray-400 rounded-md pl-3" 
+        						/>
 								</FormControl>
+
 								<FormMessage className='text-red-500' />
 							</FormItem>
 						)}
@@ -134,12 +149,12 @@ function ContactForm({ onSuccess }: { onSuccess: () => void }) {
 								<Select
 									onValueChange={field.onChange}
 									defaultValue={field.value}>
-									<FormControl>
+									<FormControl className='w-full'>
 										<SelectTrigger>
 											<SelectValue placeholder='Select an inquiry type' />
 										</SelectTrigger>
 									</FormControl>
-									<SelectContent className='w-full inline-block bg-[#E4E5E9] border border-gray-400'>
+									<SelectContent className='bg-[#fff] border border-gray-400'>
 										<SelectItem value='Project Inquiry' className='text-gray-900'>
 											Project Inquiry
 										</SelectItem>
@@ -190,7 +205,7 @@ function ContactForm({ onSuccess }: { onSuccess: () => void }) {
 // 3. The Success Message Component
 function SuccessMessage() {
 	return (
-		<div className='bg-[#E4E5E9] p-8 rounded-2xl shadow-lg border border-gray-100 flex flex-col gap-4 md:gap-8 items-center text-center'>            
+		<div className='bg-[#FFFFFF] p-8 rounded-2xl shadow-lg border border-gray-100 flex flex-col gap-4 md:gap-8 items-center text-center'>            
             <Image src="/assets/icons/confirm.png" alt='Form submission icon' width={100} height={100} className='w-16 h-16' />
 			<h3 className='text-2xl text-black font-semibold mb-4'>
 				Your Message Has Been Sent!
@@ -207,7 +222,7 @@ function SuccessMessage() {
 			<div className='flex flex-col  gap-4 w-full'>
 				<Button
 					asChild
-					className='w-full bg-[#03444A] hover:bg-[#03444A]'>
+					className='w-full text-white bg-[#03444A] hover:bg-[#03444A]'>
 					<Link href='/blog'>Read Our Blog</Link>
 				</Button>
 				<Button asChild variant='outline' className='w-full dark:bg-white dark:text-gray-900'>
@@ -223,16 +238,17 @@ export default function ContactUs() {
 	const [isSubmitted, setIsSubmitted] = useState(false)
 
 	return (
-		<section className='relative bg-[#E4E5E9] py-24 sm:py-32'>
-			<div className='absolute inset-0 flex justify-around opacity-75 pointer-events-none'>
+		<section className='relative bg-[#FFFFFF] py-24 sm:py-32'>
+			{/* <div className='absolute inset-0 flex justify-around opacity-75 pointer-events-none'>
 				{[...Array(6)].map((_, i) => (
 					<div
 						key={i}
-						className='w-px bg-dotted-line-y'
-						style={{ backgroundSize: '1px 8px' }}
+						className='w-px bg-[#D9D9D9]'
+						style={{ backgroundSize: '2px 8px' }}
 					/>
 				))}
-			</div>
+			</div> */}
+			<BackgroundEffect lineColor='gray-200' />
 
 			<div className='relative mx-auto lg:max-w-5xl xl:max-w-6xl px-4 sm:px-0'>
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-16 items-center'>
