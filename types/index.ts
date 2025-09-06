b578a0df260acb1e2513f9a93859ed30c2801a4e
touraye <touraye07@gmail.com>
2025-09-06 12:29:36 +0000

    update types

--- types/index.ts
+++ types/index.ts
@@ -17,7 +17,9 @@ export interface Event {
     slug: string;
     title: string;
     date: string; // CRITICAL: This must be a string for serialization
+    endDate?: string; // OPTIONAL: Some events has start date and end date
     formatAndLocation: string; // A combined string for display
+    location?: string; // OPTIONAL: Some events do not have location listed
     summary: string;
     coverImage: string | StaticImageData;
     isPast?: boolean; // This is calculated on the server, not passed to client
