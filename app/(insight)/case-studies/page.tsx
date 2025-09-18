268b434a1dec03945236667aa89188e5a532dc3a
touraye <touraye07@gmail.com>
2025-09-18 09:03:36 +0000

    fetch data from mdx content

--- app/(insight)/case-studies/page.tsx
+++ app/(insight)/case-studies/page.tsx
@@ -2,8 +2,11 @@ import CaseStudiesHero from "@/components/case-studies/hero";
 import CTA from "@/components/home/cta";
 import PaginatedPostGrid from "@/components/PaginatedPostGrid";
 import { mockCaseStudies } from "@/data/mock-case-studies";
+import { getAllContent } from "@/lib/content";
+import { Post } from "@/types";
 
-export default function BlogPage() {
+export default function CaseStudiesPage() {
+  const getAllPost = getAllContent<Post>('case-studies')
     return (<>
         {/* hero showing featured event */}
         <CaseStudiesHero />
@@ -11,8 +14,7 @@ export default function BlogPage() {
         {/* blog listing */}                
         <PaginatedPostGrid 
           title="Case Studies" 
-          initialPosts={mockCaseStudies} 
-          postType="case-study"
+          initialPosts={getAllPost}           
         />
 
         {/* Cta - reusable */}
